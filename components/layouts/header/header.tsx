b0e4acf9f6ee03ccce5088cd1bb6420c2851cae6
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2024-06-29 17:19:14 +0600

    add: collapsed button

--- components/layouts/header/header.tsx
+++ components/layouts/header/header.tsx
@@ -16,25 +16,36 @@ const Header = () => {
         <div className="flex-none flex items-center gap-4 ">
           <div className="flex items-center justify-between py-4 px-4 relative z-50">
             <SiteLogo className="flex-none" />
-            <div className={cn("pl-28 transition-all duration-300", {
-              "pl-10": collapsed
+            <div className={cn("ms-28 transition-all duration-300", {
+              "ms-10": collapsed
             })}>
               <div
-                className={cn("flex flex-col  justify-center cursor-pointer gap-[3px] transition-all duration-300 w-7 h-7 rounded-full bg-primary/10", {
-                  "": collapsed
+                className={cn("flex flex-col items-center justify-center cursor-pointer gap-0.5 transition-all duration-300  w-7 h-7 rounded bg-primary/10 hover:bg-primary/20", {
+                  "bg-primary/20": collapsed
                 })}
                 onClick={() => setCollapsed(!collapsed)}
               >
-                <span className="inline-block h-0.5 w-4 bg-primary"></span>
-                <span className="inline-block h-0.5 w-2.5 bg-primary"></span>
-                <span className="inline-block h-0.5 w-4 bg-primary"></span>
+                <span className={cn("inline-block h-0.5 w-4 bg-primary/60",
+                  {
+                    "rotate-45 w-3 translate-x-1 bg-primary": collapsed
+                  }
+                )}></span>
+                <span className={cn("inline-block h-0.5 w-4 bg-primary/60",
+                  {
+                    "bg-primary": collapsed
+                  }
+                )}></span>
+                <span className={cn("inline-block h-0.5 w-4 bg-primary/60",
+                  {
+                    "-rotate-45 w-3 translate-x-1 -mt-[1px] bg-primary": collapsed
+                  }
+                )}></span>
               </div>
             </div>
           </div>
           <SearchContent />
         </div>
         <div className="flex-1 flex items-center  gap-4 justify-end">
-
           <Notification />
           <GrayScaleButton />
           <ThemeButton />
