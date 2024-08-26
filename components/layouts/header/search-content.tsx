64d14e7532877bd81cf1836678556c06602937f7
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2024-08-26 16:51:28 +0600

    feat: search

--- components/layouts/header/search-content.tsx
+++ components/layouts/header/search-content.tsx
@@ -1,10 +1,43 @@
-
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command"
 import { Search } from "lucide-react"
 const SearchContent = () => {
   return (
-    <div>
-      <Search className="w-5 h-5 text-default-700" />
-    </div>
+    <Dialog>
+      <DialogTrigger>   <Search className="w-5 h-5 text-default-700" /></DialogTrigger>
+      <DialogContent className="p-0">
+        <Command>
+          <CommandInput placeholder="Type a command or search..." />
+          <CommandList className="px-2">
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup heading="Suggestions">
+              <CommandItem>Calendar</CommandItem>
+              <CommandItem>Search Emoji</CommandItem>
+              <CommandItem>Calculator</CommandItem>
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Settings">
+              <CommandItem>Profile</CommandItem>
+              <CommandItem>Billing</CommandItem>
+              <CommandItem>Settings</CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </DialogContent>
+    </Dialog>
+
   );
 };
 
