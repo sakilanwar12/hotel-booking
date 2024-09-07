7fcbe7ce8bbded186588a073e0433661bc292628
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2024-09-07 13:01:03 +0600

    feat: dashboard home page

--- app/(protected)/dashboard/orders-table.tsx
+++ app/(protected)/dashboard/orders-table.tsx
@@ -7,30 +7,55 @@ import {
     TableRow,
 } from "@/components/ui/table"
 import { Order } from "./data";
+import { Checkbox } from "@/components/ui/checkbox";
+import { SquarePen, Trash2 } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface OrdersProps {
     orders: Order[]
 }
 
+const getStatusColor = (status: "Shipped" | "Pending" | "Delivered" | "Cancelled" | "Processing") => {
+    const statusColors = {
+        "Shipped": "bg-primary",
+        "Pending": "bg-info",
+        "Delivered": "bg-success",
+        "Cancelled": "bgdestructive",
+        "Processing": "bg-warning"
+    }
+    return statusColors[status];
+}
 const OrdersTable = ({ orders }: OrdersProps) => {
     return (
         <Table>
             <TableHeader>
                 <TableRow>
-                    <TableHead>Product Name</TableHead>
-                    <TableHead>Area</TableHead>
-                    <TableHead>Price</TableHead>
-                    <TableHead className="text-right">Sales</TableHead>
+                    <TableHead><Checkbox /></TableHead>
+                    <TableHead>Order ID</TableHead>
+                    <TableHead>date</TableHead>
+                    <TableHead>amount</TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead className="text-right">Actions</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
                 {
                     orders.map((item, index) => (
                         <TableRow key={index} className="last:border-0">
+                            <TableCell><Checkbox /></TableCell>
                             <TableCell className="font-medium">{item.orderId}</TableCell>
                             <TableCell>{item.date}</TableCell>
                             <TableCell>{item.amount}</TableCell>
-                            <TableCell className="text-right">{item.status}</TableCell>
+                            <TableCell>
+                                <div className="flex items-center gap-1.5">
+                                    <div className={cn("h-2 w-2 rounded-full bg-primary", getStatusColor(item.status))}></div>
+                                    {item.status}
+                                </div>
+                            </TableCell>
+                            <TableCell className="flex justify-end items-center gap-2.5">
+                                <SquarePen className="h-4 w-4 text-default-500 cursor-pointer hover:text-primary" />
+                                <Trash2 className="h-4 w-4 text-default-500 cursor-pointer hover:text-destructive" />
+                            </TableCell>
                         </TableRow>
                     ))
                 }
